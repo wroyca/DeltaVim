ad96e5b8a483ec9e5e909aa606d7a6255d9817c8
loichyan <loichyan@foxmail.com>
2023-02-24 18:00:26 +0800

    feat(util): add `get_root()` and `get_cwd()`

--- lua/deltavim/util.lua
+++ lua/deltavim/util.lua
@@ -96,4 +96,53 @@ function M.merge_tables(dst, ...)
   return dst
 end
 
+M.ROOT_PATTERNS = { ".git" }
+
+---Returns the root directory based on:
+---* lsp workspace folders
+---* lsp root_dir
+---* root pattern of filename of the current buffer
+---* root pattern of cwd
+---Credit: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/util/init.lua
+---@return string
+function M.get_root()
+  ---@type string?
+  local path = vim.api.nvim_buf_get_name(0)
+  path = path ~= "" and vim.loop.fs_realpath(path) or nil
+  ---@type string[]
+  local roots = {}
+  if path then
+    for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+      local workspace = client.config.workspace_folders
+      local paths = workspace
+          and vim.tbl_map(
+            function(ws) return vim.uri_to_fname(ws.uri) end,
+            workspace
+          )
+        or client.config.root_dir and { client.config.root_dir }
+        or {}
+      for _, p in ipairs(paths) do
+        local r = vim.loop.fs_realpath(p)
+        if r and path:find(r, 1, true) then roots[#roots + 1] = r end
+      end
+    end
+  end
+  table.sort(roots, function(a, b) return #a > #b end)
+  ---@type string?
+  local root = roots[1]
+  if not root then
+    path = path and vim.fs.dirname(path) or M.get_cwd()
+    ---@type string?
+    root = vim.fs.find(M.ROOT_PATTERNS, { path = path, upward = true })[1]
+    root = root and vim.fs.dirname(root) or M.get_cwd()
+  end
+  ---@cast root string
+  return root
+end
+
+---@return string
+function M.get_cwd()
+  return vim.loop.cwd() --[[@as string]]
+end
+
 return M
