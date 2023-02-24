2c5d5339e1a80891b4cdeed8cb842ff5f8e076c9
loichyan <loichyan@foxmail.com>
2023-02-24 15:57:49 +0800

    feat(plugins): add coding plugins

--- lua/deltavim/plugins/coding.lua
+++ lua/deltavim/plugins/coding.lua
@@ -123,4 +123,87 @@ return {
       }
     end,
   },
+
+  -- Auto pairs
+  {
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    config = function(_, opts) require("mini.pairs").setup(opts) end,
+  },
+
+  -- Surround
+  {
+    "echasnovski/mini.surround",
+    keys = function(_, keys)
+      return Keymap.Collector()
+        :map_unique({
+          { "@surround.add", mode = { "n", "x" } },
+          { "@surround.delete", mode = "n" },
+          { "@surround.find", mode = "n" },
+          { "@surround.find_left", mode = "n" },
+          { "@surround.highlight", mode = "n" },
+          { "@surround.replace", mode = "n" },
+          { "@surround.update_n_lines", mode = "n" },
+        })
+        :collect_lazy(keys)
+    end,
+    opts = function()
+      local mappings = Keymap.Collector()
+        :map_unique({
+          { "@surround.add", "add" },
+          { "@surround.delete", "delete" },
+          { "@surround.find", "find" },
+          { "@surround.find_left", "find_left" },
+          { "@surround.highlight", "highlight" },
+          { "@surround.replace", "replace" },
+          { "@surround.update_n_lines", "update_n_lines" },
+        })
+        :collect_rhs_table()
+      return { mappings = mappings }
+    end,
+    config = function(_, opts) require("mini.surround").setup(opts) end,
+  },
+
+  -- Comments
+  {
+    "echasnovski/mini.comment",
+    event = "VeryLazy",
+    opts = {
+      hooks = {
+        pre = function()
+          require("ts_context_commentstring.internal").update_commentstring({})
+        end,
+      },
+    },
+    config = function(_, opts) require("mini.comment").setup(opts) end,
+  },
+  { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
+
+  -- Better text-objects
+  {
+    "echasnovski/mini.ai",
+    event = "VeryLazy",
+    dependencies = { "nvim-treesitter-textobjects" },
+    opts = function()
+      local ai = require("mini.ai")
+      return {
+        n_lines = 500,
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+          }),
+          f = ai.gen_spec.treesitter({
+            a = "@function.outer",
+            i = "@function.inner",
+          }),
+          c = ai.gen_spec.treesitter({
+            a = "@class.outer",
+            i = "@class.inner",
+          }),
+        },
+      }
+    end,
+    config = function(_, opts) require("mini.ai").setup(opts) end,
+  },
 }
