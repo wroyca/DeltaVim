535446df28f0d04465cfe7f532f05b1b899c5235
loichyan <loichyan@foxmail.com>
2023-07-07 13:49:57 +0800

    perf(treesitter): better way of loading treesitter-textobjects (LazyVim/LazyVim@09fafb94)

--- lua/deltavim/plugins/treesitter.lua
+++ lua/deltavim/plugins/treesitter.lua
@@ -1,10 +1,12 @@
 local Keymap = require("deltavim.core.keymap")
 
+local load_textobjects = false
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     version = false, -- Last release is way too old and doesn't work on Windows
     build = ":TSUpdate",
+    cmd = { "TSUpdate", "TSUpdateSync" },
     event = { "BufReadPost", "BufNewFile" },
     dependencies = { "nvim-treesitter-textobjects" },
     keys = function()
@@ -51,29 +53,36 @@ return {
         incremental_selection = { enable = true, keymaps = mappings },
       }
     end,
-    config = function(_, opts) require("nvim-treesitter.configs").setup(opts) end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+      if load_textobjects then
+        -- No need to load the plugin, if we only need its queries for mini.ai
+        if opts.textobjects then
+          for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+            if opts.textobjects[mod] and opts.textobjects[mod].enable then
+              local Loader = require("lazy.core.loader")
+              Loader.disabled_rtp_plugins["nvim-treesitter-textobjects"] = nil
+              local plugin =
+                require("lazy.core.config").plugins["nvim-treesitter-textobjects"]
+              require("lazy.core.loader").source_runtime(plugin.dir, "plugin")
+              break
+            end
+          end
+        end
+      end
+    end,
   },
 
   -- Text-objects queries used by other plugins like mini.ai
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
     init = function()
-      -- No need to load the plugin, if we only need its queries for mini.ai
-      local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
-      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-      local enabled = false
-      if opts.textobjects then
-        for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
-          if opts.textobjects[mod] and opts.textobjects[mod].enable then
-            enabled = true
-            break
-          end
-        end
-      end
-      if not enabled then
-        -- stylua: ignore
-        require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-      end
+      -- Disable rtp plugin, as we only need its queries for mini.ai. In case
+      -- other textobject modules are enabled, we will load them once
+      -- nvim-treesitter is loaded
+      -- stylua: ignore
+      require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+      load_textobjects = true
     end,
   },
 }
