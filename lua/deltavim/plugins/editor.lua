41eb5cde7aa3c0e4a5976ca00ba97efa2e95a044
loichyan <loichyan@foxmail.com>
2023-07-07 11:49:39 +0800

    fix(spectre): make spectre behave with regards to swap files (LazyVim/LazyVim@bb4a7fe4)

--- lua/deltavim/plugins/editor.lua
+++ lua/deltavim/plugins/editor.lua
@@ -146,6 +146,7 @@ return {
   {
     "nvim-pack/nvim-spectre",
     cmd = "Spectre",
+    opts = { open_cmd = "noswapfile vnew" },
     keys = function()
       -- stylua: ignore
       return Keymap.Collector()
