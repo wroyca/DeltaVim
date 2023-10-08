c867d2f320b511e703ef92c3cd261f6a2b480db7
loichyan <loichyan@foxmail.com>
2023-10-08 14:51:46 +0800

    fix(treesitter): load nvim-treesitter-textobjects

--- lua/deltavim/plugins/treesitter.lua
+++ lua/deltavim/plugins/treesitter.lua
@@ -1,6 +1,5 @@
 local Keymap = require("deltavim.core.keymap")
 
-local load_textobjects = false
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -8,7 +7,9 @@ return {
     build = ":TSUpdate",
     cmd = { "TSUpdate", "TSUpdateSync" },
     event = { "BufReadPost", "BufNewFile" },
-    dependencies = { "nvim-treesitter-textobjects" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
     keys = function()
       return Keymap.Collector()
         :map({
@@ -31,6 +32,7 @@ return {
           "c",
           "html",
           "javascript",
+          "jsdoc",
           "json",
           "lua",
           "luadoc",
@@ -54,32 +56,6 @@ return {
     end,
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
-      if load_textobjects then
-        -- No need to load the plugin, if we only need its queries for mini.ai
-        if opts.textobjects then
-          for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
-            if opts.textobjects[mod] and opts.textobjects[mod].enable then
-              local Loader = require("lazy.core.loader")
-              Loader.disabled_rtp_plugins["nvim-treesitter-textobjects"] = nil
-              local plugin = require("lazy.core.config").plugins["nvim-treesitter-textobjects"]
-              require("lazy.core.loader").source_runtime(plugin.dir, "plugin")
-              break
-            end
-          end
-        end
-      end
-    end,
-  },
-
-  -- Text-objects queries used by other plugins like mini.ai
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    init = function()
-      -- Disable rtp plugin, as we only need its queries for mini.ai. In case
-      -- other textobject modules are enabled, we will load them once
-      -- nvim-treesitter is loaded
-      require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-      load_textobjects = true
     end,
   },
 }
