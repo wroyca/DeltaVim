d8ed90e8c314961f1ca25ce3587a503dac4dcb47
William Roy <wroy@proton.me>
2023-10-19 10:23:22 -0400

    Fix deprecation warning for get_active_clients()
    https://neovim.io/doc/user/deprecated.html#vim.lsp.get_active_clients()

--- lua/deltavim/util.lua
+++ lua/deltavim/util.lua
@@ -10,7 +10,7 @@ function M.get_lsp_root(path)
   end
   ---@type string[]
   local roots = {}
-  for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+  for _, client in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
     local workspace = client.config.workspace_folders
     local paths = workspace
         and vim.tbl_map(function(ws)
