e37aca5a051c574f5d01c510623a6aa4de365e9f
Loi Chyan <loichyan@foxmail.com>
2024-05-06 19:08:14 +0800

    fix(resession): silent errors

--- lua/deltavim/mappings/resession.lua
+++ lua/deltavim/mappings/resession.lua
@@ -13,19 +13,21 @@ return {
   },
 
   load = {
-    function() require("resession").load() end,
+    function() require("resession").load(nil, { silence_errors = true }) end,
     desc = "Load a session",
   },
   load_last = {
-    function() require("resession").load "last" end,
+    function() require("resession").load("last", { silence_errors = true }) end,
     desc = "Load last session",
   },
   load_dir = {
-    function() require("resession").load(nil, { dir = "dirsession" }) end,
+    function() require("resession").load(nil, { dir = "dirsession", silence_errors = true }) end,
     desc = "Load a dirsession",
   },
   load_dir_cwd = {
-    function() require("resession").load(vim.fn.getcwd(), { dir = "dirsession" }) end,
+    function()
+      require("resession").load(vim.fn.getcwd(), { dir = "dirsession", silence_errors = true })
+    end,
     desc = "Load current dirsession",
   },
 
@@ -34,7 +36,7 @@ return {
     desc = "Delete a session",
   },
   delete_dir = {
-    function() require("resession").delete(nil, { dir = "dirsession" }) end,
+    function() require("resession").delete(nil, { dir = "dirsession", silence_errors = true }) end,
     desc = "Delete a dirsession",
   },
 }
