a61039d5f8da65646693b8dbc0755fa804cbe4c6
Loi Chyan <loichyan@foxmail.com>
2024-05-08 16:18:11 +0800

    fix(smart-splits): silent resize mode

--- lua/deltavim/plugins/utility.lua
+++ lua/deltavim/plugins/utility.lua
@@ -43,6 +43,7 @@ return {
     "mrjones2014/smart-splits.nvim",
     lazy = true,
     opts = {
+      resize_mode = { quit_key = "q", silent = true },
       ignored_filetypes = { "nofile", "quickfix", "qf", "prompt" },
       ignored_buftypes = { "nofile" },
     },
