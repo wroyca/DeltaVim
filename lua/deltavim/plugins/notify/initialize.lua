51a09fbd56a1f4eecc8a440fac2f4161c27b2561
Loi Chyan <loichyan@foxmail.com>
2024-05-10 15:57:10 +0800

    fix: notification deferment not playing nicely with `nvim-notify`
    Credit: AstroNvim/AstroNvim@d45edb12bcc8f3709ae77ada53b2c72daf7f683e

--- lua/deltavim/plugins/notify/initialize.lua
+++ lua/deltavim/plugins/notify/initialize.lua
@@ -1,1 +1,5 @@
-return function() require("astrocore").load_plugin_with_func("nvim-notify", vim, "notify") end
+return function()
+  local lazy_notify = require "astronvim.lazy_notify"
+  if vim.notify == lazy_notify.queue then vim.notify = lazy_notify.original end
+  require("astrocore").load_plugin_with_func("nvim-notify", vim, "notify")
+end
