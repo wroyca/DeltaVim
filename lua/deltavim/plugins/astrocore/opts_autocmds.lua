2288e9d5425199ca4e7455728024b9e8047948b3
Loi Chyan <loichyan@foxmail.com>
2024-05-10 15:39:29 +0800

    fix: make `git` optional in the path for execution
    Credit: AstroNvim/AstroNvim@b10119d295e369fabf46d2648b0ab969cbc5c0bc

--- lua/deltavim/plugins/astrocore/opts_autocmds.lua
+++ lua/deltavim/plugins/astrocore/opts_autocmds.lua
@@ -171,10 +171,12 @@ return {
           astro.event "File"
 
           -- check git repo and fire AstroGitFile
-          local folder = vim.fn.fnamemodify(current_file, ":p:h")
-          if vim.fn.has "win32" == 1 then folder = ('"%s"'):format(folder) end
-          if astro.cmd({ "git", "-C", folder, "rev-parse" }, false) or astro.file_worktree() then
-            astro.event "GitFile"
+          if vim.fn.executable "git" == 1 then
+            local folder = vim.fn.fnamemodify(current_file, ":p:h")
+            if vim.fn.has "win32" == 1 then folder = ('"%s"'):format(folder) end
+            if astro.cmd({ "git", "-C", folder, "rev-parse" }, false) or astro.file_worktree() then
+              astro.event "GitFile"
+            end
           end
 
           -- trigger original events
