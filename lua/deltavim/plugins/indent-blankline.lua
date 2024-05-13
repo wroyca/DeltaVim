22ef8801188ac353b47625b5b632be706ff3375b
Loi Chyan <loichyan@foxmail.com>
2024-05-13 15:59:21 +0800

    fix(indent-blankline): make `indent.tab_char` same as `indent.char`

--- lua/deltavim/plugins/indent-blankline.lua
+++ lua/deltavim/plugins/indent-blankline.lua
@@ -13,7 +13,7 @@ return {
   },
   ---@type ibl.config
   opts = {
-    indent = { char = "▏" },
+    indent = { char = "▏", tab_char = "▏" },
     scope = {
       enabled = true,
       show_start = false,
