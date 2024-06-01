6452e90a4889b21aaaaefedf7e5fc59aebd74a2c
Loi Chyan <loichyan@foxmail.com>
2024-06-01 17:59:00 +0800

    fix(ts-autotag): remove deprecated treesitter setup

--- lua/deltavim/plugins/ts-autotag.lua
+++ lua/deltavim/plugins/ts-autotag.lua
@@ -2,9 +2,7 @@
 return {
   "windwp/nvim-ts-autotag",
   event = "User AstroFile",
-  dependencies = {
-    { "nvim-treesitter", opts = { autotag = { enable = true } } },
-  },
+  dependencies = { "nvim-treesitter" },
   config = function(_, opts)
     require("nvim-ts-autotag").setup(opts)
     require("astrocore").exec_buffer_autocmds("FileType", { group = "nvim_ts_xmltag" })
