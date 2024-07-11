41a4ad589d7cb0865122bc3cb49d19091bac5117
Loi Chyan <loichyan@foxmail.com>
2024-07-11 18:16:15 +0800

    refactor(autopairs): disable autopairs.nvim integration with nvim-cmp

--- lua/deltavim/plugins/autopairs.lua
+++ lua/deltavim/plugins/autopairs.lua
@@ -10,16 +10,6 @@ return {
   config = function(_, opts)
     local npairs, astro = require "nvim-autopairs", require "astrocore"
     npairs.setup(opts)
-
     if not astro.config.features.autopairs then npairs.disable() end
-    astro.on_load(
-      "nvim-cmp",
-      function()
-        require("cmp").event:on(
-          "confirm_done",
-          require("nvim-autopairs.completion.cmp").on_confirm_done { tex = false }
-        )
-      end
-    )
   end,
 }
