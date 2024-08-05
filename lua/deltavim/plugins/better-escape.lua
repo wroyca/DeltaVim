0f74651ab084dc27e297c2691f299a9d7397d9b4
Loi Chyan <loichyan@foxmail.com>
2024-08-05 10:23:23 +0800

    fix(better-escape): only use `jj` and `jk` for escape in insert mode
    Credit: AstroNvim/AstroNvim@08399f6ead60

--- lua/deltavim/plugins/better-escape.lua
+++ lua/deltavim/plugins/better-escape.lua
@@ -2,5 +2,11 @@
 return {
   "max397574/better-escape.nvim",
   event = "VeryLazy",
-  opts = { timeout = 300 },
+  opts = {
+    timeout = 300,
+    default_mappings = false,
+    mappings = {
+      i = { j = { k = "<Esc>", j = "<Esc>" } },
+    },
+  },
 }
