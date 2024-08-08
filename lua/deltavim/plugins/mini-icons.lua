1539247218ba0a00c9629356779e3940f6a5ac90
William Roy <wroy@proton.me>
2024-08-08 18:44:14 -0400

    feat(plugins): migrate from web-devicons to mini-icons

---
+++ lua/deltavim/plugins/mini-icons.lua
@@ -0,0 +1,10 @@
+---@type LazyPluginSpec
+return {
+  "echasnovski/mini.icons",
+  event = "VeryLazy",
+  config = function(_, opts)
+    local icons = require "mini.icons"
+    icons.setup(opts)
+    icons.mock_nvim_web_devicons()
+  end,
+}
