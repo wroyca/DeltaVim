0dae7a9401ad1e275c816ea8259c3a08025cdfd7
William Roy <wroy@proton.me>
2024-09-13 00:33:04 -0400

    refactor(bootstrap): set preferred colorscheme on install (#21)
    Tweak default Lazy options and set preferred colorscheme on install.

--- plugin/lazy.lua
+++ plugin/lazy.lua
@@ -1,12 +1,28 @@
+---@module "lazy"
+
 if package.loaded["lazy"] then
-  -- Note that lazy.nvim overrides Neovim's built-in loader, causing
-  -- pack/*/start/* to be sourced again. The overhead is minimal since we mark
-  -- the file as no-op after lazy.nvim loads (using package.loaded).
+  -- Keep plugin management separate from configuration logic.
   --
-  -- https://github.com/folke/lazy.nvim/issues/1180.
+  -- Note that lazy.nvim overrides Neovim's built-in loader, causing
+  -- pack/*/start/* to be sourced again. The overhead is minimal since the
+  -- files are marked as no-op after lazy.nvim loads (using package.loaded).
+  --
+  -- For more details, see: https://github.com/folke/lazy.nvim/issues/1180
   --
   return
 end
+
+-- Neovim 0.10.1 includes logic to test whether a given terminal emulator
+-- supports truecolor and sets the default value of `termguicolors` to true.
+--
+-- Unfortunately, the latter method can cause a "flash" visual effect in some
+-- terminals (e.g., macOS's Terminal.app / nsterm) as truecolor is enabled and
+-- then disabled again. It is also exacerbated by blocking operations (e.g.,
+-- plugin manager setup). This behavior is suboptimal.
+--
+-- For more details, see: https://github.com/neovim/neovim/issues/29966
+--
+vim.o.termguicolors = true
 
 local lazypath = vim.fs.joinpath(vim.fn.stdpath "data" --[[ @as string ]], "lazy", "lazy.nvim")
 if not vim.uv.fs_stat(lazypath) then
@@ -22,49 +38,25 @@ if not vim.uv.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local deltavim = require "deltavim"
-deltavim.setup {
-  mapleader = " ",
-  maplocalleader = "\\",
-  icons_enabled = true,
-  pin_plugins = vim.env.DELTA_PIN_PLUGINS == "1",
-}
-
-require("lazy").setup {
-  spec = {
-    {
-      "loichyan/DeltaVim",
-      priority = 1000,
-    },
-    { import = "deltavim.plugins" },
-    { import = "deltavim.presets.mappings" },
-  },
-
-  dev = {
-    path = vim.fn.stdpath "config",
-    patterns = { "loichyan" },
-    fallback = false,
-  },
-
-  -- These either have a direct replacement (e.g., matchit -> matchup) or
-  -- are just straight-up obsolete.
+---@type LazyConfig
+local opts = {
   performance = {
     rtp = {
       disabled_plugins = {
         "2html_plugin",
         "bugreport",
         "ftplugin",
-        "getscriptPlugin",
         "getscript",
+        "getscriptPlugin",
         "gzip",
         "health",
         "logipat",
         "matchit",
         "matchparen",
+        "netrw",
         "netrwFileHandlers",
         "netrwPlugin",
         "netrwSettings",
-        "netrw",
         "nvim",
         "optwin",
         "rplugin",
@@ -73,36 +65,58 @@ require("lazy").setup {
         "spellfile_plugin",
         "synmenu",
         "syntax",
-        "tarPlugin",
         "tar",
+        "tarPlugin",
         "tohtml",
         "tutor",
-        "vimballPlugin",
         "vimball",
-        "zipPlugin",
+        "vimballPlugin",
         "zip",
+        "zipPlugin",
       },
     },
   },
 
-  -- https://github.com/folke/lazy.nvim/issues/1008
-  change_detection = {
+  defaults = {
+    lazy = false,
+    version = false,
+  },
+
+  pkg = {
     enabled = false,
   },
 
-  -- lazy can generate helptags from the headings in markdown readme files, so
-  -- :help works even for plugins that don't have vim docs. In practise though,
-  -- most README aren't made to be appropriate documentation (beyond the basic
-  -- setup), so this may confuse the average users.
   readme = {
     enabled = false,
   },
 
-  -- Using a direct configuration often means that the user just wants an
-  -- out-of-the-box experience and pills show categories that are (IMO) more
-  -- geared toward plugin developers in general. Chances are that this is not
-  -- interesting for average users.
+  change_detection = {
+    enabled = false,
+  },
+
+  install = {
+    colorscheme = { "astrotheme", "habamax" },
+  },
+
   ui = {
     pills = false,
+    border = "single",
+    backdrop = 100,
+  },
+
+  dev = {
+    path = vim.fs.joinpath(os.getenv "HOME" or os.getenv "USERPROFILE", "Projects"),
+    patterns = {
+      os.getenv "USER" or os.getenv "USERNAME",
+    },
   },
 }
+
+---@type LazyPluginSpec[]
+local spec = {
+  { "loichyan/DeltaVim" },
+  { import = "deltavim.plugins" },
+  { import = "deltavim.presets.mappings" },
+}
+
+require("lazy").setup(spec, opts)
